7db51980ea79bdead0d4c29e5ebd1a947a38363b
Magda <zyla.magdalena.92@gmail.com>
2023-02-05 17:12:13 +0100

    Day 4

--- AppC7/AppC7/Program.cs
+++ AppC7/AppC7/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool woman = true;
+int age = 29;
+
+if (woman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && woman == false)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Another person");
+}
